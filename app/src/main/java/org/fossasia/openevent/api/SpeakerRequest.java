07745c4f7647f40310cc51a6f679eb447680ec96
Arnav Gupta <championswimmer@gmail.com>
2015-05-23 20:39:29 +0530

    add speaker and sponsor request methods
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/org/fossasia/openevent/api/SpeakerRequest.java
+++ app/src/main/java/org/fossasia/openevent/api/SpeakerRequest.java
@@ -2,9 +2,12 @@ package org.fossasia.openevent.api;
 
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
 import org.fossasia.openevent.data.Speaker;
 import org.fossasia.openevent.data.Sponsor;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -18,8 +21,21 @@ public class SpeakerRequest extends JsonObjectRequest {
         super(Method.GET, Urls.Get.SPEAKERS, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                //TODO: Turn json response into arraylist of Events
-                listener.onEventsFetched(new ArrayList<Speaker>());
+                try {
+                    JSONArray speakersArray = response.getJSONArray("speakers");
+                    ArrayList<Speaker> speakers = new ArrayList<Speaker>(speakersArray.length());
+                    Speaker s;
+                    Gson g = new Gson();
+
+                    for (int i = 0; i < speakersArray.length(); i++) {
+                        s = g.fromJson(speakersArray.getJSONObject(i).toString(), Speaker.class);
+                        speakers.add(s);
+                    }
+
+                    listener.onEventsFetched(speakers);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         },
         errorListener);
--- app/src/main/java/org/fossasia/openevent/api/SponsorRequest.java
+++ app/src/main/java/org/fossasia/openevent/api/SponsorRequest.java
@@ -2,9 +2,13 @@ package org.fossasia.openevent.api;
 
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
 import org.fossasia.openevent.data.Sponsor;
+import org.fossasia.openevent.data.Sponsor;
 import org.fossasia.openevent.data.Track;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -18,8 +22,21 @@ public class SponsorRequest extends JsonObjectRequest {
         super(Method.GET, Urls.Get.SPONSORS, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                //TODO: Turn json response into arraylist of Events
-                listener.onEventsFetched(new ArrayList<Sponsor>());
+                try {
+                    JSONArray sponsorsArray = response.getJSONArray("sponsors");
+                    ArrayList<Sponsor> sponsors = new ArrayList<Sponsor>(sponsorsArray.length());
+                    Sponsor s;
+                    Gson g = new Gson();
+
+                    for (int i = 0; i < sponsorsArray.length(); i++) {
+                        s = g.fromJson(sponsorsArray.getJSONObject(i).toString(), Sponsor.class);
+                        sponsors.add(s);
+                    }
+
+                    listener.onEventsFetched(sponsors);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         },
         errorListener);
