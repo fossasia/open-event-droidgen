a7ca778527815be9216523a0e0a497995fe67b8b
mananwason <manan13056@iiitd.ac.in>
2015-08-04 19:56:39 +0530

    Splash Added, Data Download now in Background and Progress bar added

--- app/src/main/java/org/fossasia/openevent/activities/SplashActivity.java
+++ app/src/main/java/org/fossasia/openevent/activities/SplashActivity.java
@@ -3,7 +3,7 @@ package org.fossasia.openevent.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Handler;
 import android.view.Window;
 
 import org.fossasia.openevent.R;
@@ -12,14 +12,23 @@ import org.fossasia.openevent.R;
  * Created by MananWason on 10-06-2015.
  */
 public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
+
--- app/src/main/java/org/fossasia/openevent/api/processor/TrackListResponseProcessor.java
+++ app/src/main/java/org/fossasia/openevent/api/processor/TrackListResponseProcessor.java
@@ -2,10 +2,15 @@ package org.fossasia.openevent.api.processor;
 
 import android.util.Log;
 
+import com.squareup.otto.Bus;
+
+import org.fossasia.openevent.OpenEventApp;
 import org.fossasia.openevent.api.protocol.TrackResponseList;
 import org.fossasia.openevent.data.Track;
 import org.fossasia.openevent.dbutils.DbContract;
 import org.fossasia.openevent.dbutils.DbSingleton;
+import org.fossasia.openevent.events.FailedDownload;
+import org.fossasia.openevent.events.TracksDownloadEvent;
 
 import java.util.ArrayList;
 
@@ -34,10 +39,13 @@ public class TrackListResponseProcessor implements Callback<TrackResponseList> {
         dbSingleton.clearDatabase(DbContract.Tracks.TABLE_NAME);
         dbSingleton.insertQueries(queries);
 
+        Bus bus = OpenEventApp.getEventBus();
+        bus.post(new TracksDownloadEvent());
     }
 
     @Override
     public void failure(RetrofitError error) {
-
+        Bus bus = OpenEventApp.getEventBus();
+        bus.post(new FailedDownload());
     }
 }
