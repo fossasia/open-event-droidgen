8c2c9787a85315b28ed30da530597f89e0590917
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-06-20 18:12:07 +0530

    Add script to initiate download once apk has been compiled

--- apk-generator/scripts/runPy.php
+++ apk-generator/scripts/runPy.php
@@ -1,8 +1,7 @@
 <?php
-echo "Please wait while the app is being generated, it can take around 10 minutes for the build to finish.The completed app will be mailed to you ";
-if(isset($_POST['timestamp']))
 {
     $uid = $_POST['timestamp'];
     exec("sudo python /var/www/appgenserver.py $uid");
+    echo "http://192.241.232.231/release/".$uid."/releaseapk.apk";
 }
 ?>
