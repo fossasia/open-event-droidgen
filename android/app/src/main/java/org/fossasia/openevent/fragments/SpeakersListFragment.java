8fa7426f92c1d715755da418ceea945466a6629e
Arpit Aggarwal <arpitdec5@gmail.com>
2017-01-18 18:25:50 +0530

    fixes issue #958 (#1008)
    * made changes to the speaker and schedule fragment
    
    * made changes to speaker fragment

--- android/app/src/main/java/org/fossasia/openevent/fragments/SpeakersListFragment.java
+++ android/app/src/main/java/org/fossasia/openevent/fragments/SpeakersListFragment.java
@@ -12,11 +12,12 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,7 +26,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
-
 
 import com.squareup.otto.Subscribe;
 
@@ -69,9 +69,6 @@ public class SpeakersListFragment extends BaseFragment implements SearchView.OnQ
     private int sortType;
 
     private Snackbar snackbar;
-
-
-    private StaggeredGridLayoutManager gridLayoutManager;
     private Toolbar toolbar;
     private AppBarLayout.LayoutParams layoutParams;
     private int SCROLL_OFF = 0;
@@ -91,18 +88,23 @@ public class SpeakersListFragment extends BaseFragment implements SearchView.OnQ
         prefsSort = PreferenceManager.getDefaultSharedPreferences(getActivity());
         sortType = prefsSort.getInt(PREF_SORT, 0);
 
-        speakersListAdapter = new SpeakersListAdapter(mSpeakers, getActivity());
+        //setting the grid layout to cut-off white space in tablet view
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        float width = displayMetrics.widthPixels / displayMetrics.density;
+        int spanCount = (int) (width/150.00);
+
         speakersRecyclerView.addItemDecoration(new MarginDecoration(getContext()));
         speakersRecyclerView.setHasFixedSize(true);
+        speakersListAdapter = new SpeakersListAdapter(mSpeakers, getActivity());
         speakersRecyclerView.setAdapter(speakersListAdapter);
-        gridLayoutManager = new StaggeredGridLayoutManager(spanCount,StaggeredGridLayoutManager.VERTICAL);
+        final GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),spanCount);
         speakersRecyclerView.setLayoutManager(gridLayoutManager);
         speakersRecyclerView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
             public boolean onPreDraw() {
                 toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
                 layoutParams = (AppBarLayout.LayoutParams) toolbar.getLayoutParams();
-                if (mSpeakers.size() - 1 - gridLayoutManager.findLastCompletelyVisibleItemPositions(new int[spanCount])[0] < spanCount ) {
+                if (gridLayoutManager.findLastVisibleItemPosition() == mSpeakers.size()-1) {
                     layoutParams.setScrollFlags(SCROLL_OFF);
                     toolbar.setLayoutParams(layoutParams);
                 }
