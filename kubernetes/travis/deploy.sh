5a3090adb03481623f74680b24a87b6417403a44
Niranjan Rajendran <niranjan94@yahoo.com>
2017-01-24 18:35:22 +0530

    Updated google cloud service account key

--- kubernetes/travis/deploy.sh
+++ kubernetes/travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DEPLOY_BRANCH = ${DEPLOY_BRANCH:-master}
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-android" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Just a PR. Skip google cloud deployment."
@@ -20,10 +20,10 @@ gcloud components install kubectl
 
 gcloud config set compute/zone us-west1-a
 # Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
-openssl aes-256-cbc -K $encrypted_27e15b7757b4_key -iv $encrypted_27e15b7757b4_iv -in ./kubernetes/travis/eventyay-8245fde7ab8a.json.enc -out eventyay-8245fde7ab8a.json -d
+openssl aes-256-cbc -K $encrypted_089594c81b43_key -iv $encrypted_089594c81b43_iv -in ./kubernetes/travis/eventyay-b6f445785c27.json.enc -out eventyay-b6f445785c27.json -d
 mkdir -p lib
-gcloud auth activate-service-account --key-file eventyay-8245fde7ab8a.json
-export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/eventyay-8245fde7ab8a.json
+gcloud auth activate-service-account --key-file eventyay-b6f445785c27.json
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/eventyay-b6f445785c27.json
 gcloud config set project eventyay
 gcloud container clusters get-credentials eventyay-cluster
 cd kubernetes/images/generator
