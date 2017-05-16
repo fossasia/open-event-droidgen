8bfc6b5acc0d33d21390a7398522ec2aa3dc13ab
Niranjan Rajendran <me@niranjan.io>
2017-05-16 12:41:34 +0530

    Push docker images to public docker repo

--- kubernetes/travis/deploy.sh
+++ kubernetes/travis/deploy.sh
@@ -25,7 +25,8 @@ export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/eventyay-b6f445785c27.json
 gcloud config set project eventyay
 gcloud container clusters get-credentials staging-cluster
 cd kubernetes/images/generator
-docker build --build-arg COMMIT_HASH=$COMMIT_HASH --build-arg BRANCH=$BRANCH --build-arg REPOSITORY=$REPOSITORY --no-cache -t gcr.io/eventyay/generators/android:$COMMIT_HASH .
-docker tag gcr.io/eventyay/generators/android:$COMMIT_HASH gcr.io/eventyay/generators/android:latest
-gcloud docker -- push gcr.io/eventyay/generators/android
-kubectl set image deployment/android-generator android-generator=gcr.io/eventyay/generators/android:$COMMIT_HASH
+docker build --build-arg COMMIT_HASH=$COMMIT_HASH --build-arg BRANCH=$BRANCH --build-arg REPOSITORY=$REPOSITORY --no-cache -t eventyay/android-generator:$COMMIT_HASH .
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker tag eventyay/android-generator:$COMMIT_HASH eventyay/android-generator:latest
+docker push eventyay/android-generator
+kubectl set image deployment/android-generator android-generator=eventyay/android-generator:$COMMIT_HASH
