a4666ec57f7e3c20c670521a2e6b022b338b0c6f
Areeb Jamal <jamal.areeb@gmail.com>
2017-06-07 11:08:48 +0530

    chore: Move Date tests from androidTest/ to test/ (#1669)
    * chore: Move Date tests from androidTest/ to test/
    
    * corrected DateTest.java file (#3)

--- android/app/src/test/java/org/fossasia/openevent/utils/DateTest.java
+++ android/app/src/test/java/org/fossasia/openevent/utils/DateTest.java
@@ -1,6 +1,5 @@
-package org.fossasia.openevent;
+package org.fossasia.openevent.utils;
 
-import org.fossasia.openevent.utils.ISO8601Date;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -51,7 +50,7 @@ public class DateTest {
         String actualString;
 
         actualString = ISO8601Date.getTimeZoneDateStringFromString(dateString);
-        expectedString = "Thu, 01 Jun 2017, 20:29, GMT+02:00";
+        expectedString = "Thu, 01 Jun 2017, 20:29, CEST";
         Assert.assertEquals(expectedString, actualString);
 
         //second case
@@ -59,7 +58,7 @@ public class DateTest {
         ISO8601Date.setEventTimeZone("Asia/Singapore");
 
         actualString = ISO8601Date.getTimeZoneDateStringFromString(dateString);
-        expectedString = "Thu, 01 Jun 2017, 23:59, GMT+05:30";
+        expectedString = "Thu, 01 Jun 2017, 23:59, IST";
         Assert.assertEquals(expectedString, actualString);
 
         //third case
@@ -67,7 +66,7 @@ public class DateTest {
         ISO8601Date.setEventTimeZone("Asia/Singapore");
 
         actualString = ISO8601Date.getTimeZoneDateStringFromString(dateString);
-        expectedString = "Thu, 01 Jun 2017, 20:29, GMT+02:00";
+        expectedString = "Thu, 01 Jun 2017, 20:29, CEST";
         Assert.assertEquals(expectedString, actualString);
 
         //fourth case
@@ -75,7 +74,7 @@ public class DateTest {
         ISO8601Date.setEventTimeZone("Asia/Singapore");
 
         actualString = ISO8601Date.getTimeZoneDateStringFromString(dateString);
-        expectedString = "Fri, 02 Jun 2017, 04:29, GMT+10:00";
+        expectedString = "Fri, 02 Jun 2017, 04:29, AEST";
         Assert.assertEquals(expectedString, actualString);
     }
 
