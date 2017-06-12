b2d1e534320229a1b73e9a77a22077d3c58d0e76
Shreyas Bhat <shreyasnbhat@gmail.com>
2017-06-12 18:59:46 +0530

    chore: Remove Bookmark Fragment and fragment_bookmarks.xml (#1698)

--- android/app/src/main/java/org/fossasia/openevent/utils/BookmarksListChangeListener.java
+++ android/app/src/main/java/org/fossasia/openevent/utils/BookmarksListChangeListener.java
@@ -1,7 +1,7 @@
 package org.fossasia.openevent.utils;
 
 /*
-* Interface to connect BookmarksFragment and SessionsListAdapter.
+* Interface to connect AboutFragment and SessionsListAdapter.
 * onChange Method is called by the onClick method
 * of Positive Button of the 'Remove Bookmark' Dialog
 * in the onBindViewHolder method insideSessionsListAdapter
