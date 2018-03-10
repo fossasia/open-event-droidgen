a83c7421255130a7250e67eef6cf252f28659801
Parthiv Mangukiya <ParthivMangukiya@users.noreply.github.com>
2018-03-10 19:31:37 +0530

    fix: Add correct path for release-unsigned.apk according to buildtype (#2360)

--- apk-generator/v2/scripts/build_apk.sh
+++ apk-generator/v2/scripts/build_apk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./gradlew assemble$2Release --info
 echo "signing"
-jarsigner -keystore ${KEYSTORE_PATH} -storepass ${KEYSTORE_PASSWORD} app/build/outputs/apk/app-$2-release-unsigned.apk ${KEY_ALIAS}
+jarsigner -keystore ${KEYSTORE_PATH} -storepass ${KEYSTORE_PASSWORD} app/build/outputs/apk/$2/release/app-$2-release-unsigned.apk ${KEY_ALIAS}
 echo "zipaligning"
-${1}/zipalign -v 4 app/build/outputs/apk/app-$2-release-unsigned.apk release.apk
+${1}/zipalign -v 4 app/build/outputs/apk/$2/release/app-$2-release-unsigned.apk release.apk
 echo "done"
